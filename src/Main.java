af36636d9a7862cce2c56ea0375312f694193a8d
Anton9380 <120788897+Anton9380@users.noreply.github.com>
2024-05-05 16:26:38 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,33 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-class City {
-    private String name;
-    private String region;
-    private String district;
-    private long population;
-    private String foundation;
-
-    public City(String name, String region, String district, long population, String foundation) {
-        this.name = name;
-        this.region = region;
-        this.district = district;
-        this.population = population;
-        this.foundation = foundation;
-    }
-
-    @Override
-    public String toString() {
-        return "City{" +
-                "name='" + name + '\'' +
-                ", region='" + region + '\'' +
-                ", district='" + district + '\'' +
-                ", population=" + population +
-                ", foundation='" + foundation + '\'' +
-                '}';
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
         List<City> cities = readCitiesFromFile("cities.txt");
